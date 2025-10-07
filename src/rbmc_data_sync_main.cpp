b730d47f48cc5a7d40d861ecc630a8a84b6295e5
Arya K Padman <aryakpadman@gmail.com>
2025-10-07 09:35:19 -0500

    signal handler for reloading
    a signal handler to test the reloading mechanisms of systemd.
    
    Change-Id: Ie8f5fa967866224d170049e2ab808a9af266e49f

--- src/rbmc_data_sync_main.cpp
+++ src/rbmc_data_sync_main.cpp
@@ -5,13 +5,40 @@
 #include "external_data_ifaces_impl.hpp"
 #include "manager.hpp"
 
+#include <csignal>
+#include <systemd/sd-daemon.h> // sd_notify()
+#include <thread> 
+#include <chrono> 
 #include <phosphor-logging/lg2.hpp>
 #include <sdbusplus/async/context.hpp>
 #include <sdbusplus/server/manager.hpp>
 #include <xyz/openbmc_project/Control/SyncBMCData/common.hpp>
 
+void sighupHandler(int signum)
+{
+    if (signum == SIGHUP)
+    {
+        // To inform systemd manager that reload has been initated
+        sd_notify(0, "RELOADING=1\nSTATUS=Data sync Reloadg started...");
+
+        lg2::warning("Received SIGHUP! Reloading configurations!!");
+        std::this_thread::sleep_for(std::chrono::milliseconds(3000));
+        lg2::warning("Reload finished!!!");
+
+        //Inform that reload has finished
+        sd_notify(0, "READY=1");
+    }
+}
+
 int main()
 {
+    // Register the SIGHUP handler
+    if (std::signal(SIGHUP, sighupHandler) == SIG_ERR)
+    {
+        lg2::error("Error registering SIGHUP handler");
+        return 1;
+    }
+
     using SyncBMCData =
         sdbusplus::common::xyz::openbmc_project::control::SyncBMCData;
 
@@ -28,7 +55,7 @@ int main()
         ctx.request_name(SyncBMCData::interface);
         co_return;
     }(ctx));
-
+    sd_notify(0, "READY=1");
     ctx.run();
 
     return 0;
