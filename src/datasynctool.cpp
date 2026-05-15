5de9ee4063de0d327ee57b646729c1f184585996
Arya K Padman <aryakpadman@gmail.com>
2026-05-15 00:04:14 -0500

    datasynctool: Framework for a CLI tool
    This commit introduce an initial framework for a command-line utility
    `datasynctool` for phosphor-data-sync which helps ease user
    interaction with phosphor-data-sync.
    
    Further commits will implement the various options supported by the
    tool.
    
    Change-Id: I491256fe82994ce69c5104364876fcbfef1b6cf3
    Signed-off-by: Arya K Padman <aryakpadman@gmail.com>

---
+++ src/datasynctool.cpp
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: Apache-2.0
+
+#include <CLI/CLI.hpp>
+
+#include <iostream>
+#include <string>
+
+int main(int argc, char* argv[])
+{
+    CLI::App app{"Data Sync Tool - Command line utility for phosphor-data-sync"};
+
+    // Parse command line arguments
+    try
+    {
+        app.parse(argc, argv);
+    }
+    catch (const CLI::ParseError& e)
+    {
+        return app.exit(e);
+    }
+
+    std::cout << "Data Sync Tool initialized\n";
+    std::cout << "Use --help for available options.\n";
+
+    return 0;
+}
